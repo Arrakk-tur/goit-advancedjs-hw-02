e5212196c85440e71ef692e6374e404a49addb7b
Kostiantyn Skliar <skljar.k@gmail.com>
2024-10-31 01:16:56 +0200

    Timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,95 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startButton = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+let countdownInterval;
+let selectedDate;
+
+// Використання Flatpickr
+
+// Options flatpickr
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+startButton.disabled = true;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// 0
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+// Interface Update
+function updateInterface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+// Timer
+function startTimer() {
+  startButton.disabled = true;
+
+  countdownInterval = setInterval(() => {
+    const currentTime = new Date();
+    const deltaTime = selectedDate - currentTime;
+    const timeComponents = convertMs(deltaTime);
+
+    updateInterface(timeComponents);
+
+    if (deltaTime <= 0) {
+      clearInterval(countdownInterval);
+      updateInterface({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+    }
+  }, 1000);
+}
+
+// Start button
+startButton.addEventListener('click', startTimer);
